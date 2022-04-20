3e002db50cc1b3b67a100e90021d7df762e55661
Costello1329 <leladze.kg@phystech.edu>
2022-04-20 12:59:13 +0300

    removed command line args

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,12 @@
 import testlib.core.Tester;
 
 import java.nio.file.Path;
-import java.util.Arrays;
 
 public class Main {
     public static void main (final String[] args) {
-        if (args.length != 2) {
-            System.out.println("Expected to receive 2 paths to jar archives (reference jar and solution jar)");
-            System.exit(1);
-        }
-
         Tester.test(
             Tests.class,
-            Arrays.stream(args).map(Path::of).toArray(Path[]::new),
+            new Path[] { Path.of("reference.jar"), Path.of("solution.jar") },
             new String[][] {
                 new String[] { "reference.Point", "reference.Square" },
                 new String[] { "solution.Point", "solution.Square" }
